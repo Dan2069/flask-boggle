dc13c9a9d98363e4e61d6398733570b5c1504337
Daniel <danielmosqueda15@gmail.com>
2022-07-17 02:33:42 -0400

    final

--- app.py
+++ app.py
@@ -1,32 +1,46 @@
+from flask import Flask, request, render_template, jsonify, session
 from boggle import Boggle
-from flask import Flask, render_template, redirect, session, request
-from flask_debugtoolbar import DebugToolbarExtension
-
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = "secret"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-
-debug = DebugToolbarExtension(app)
-
+app.config["SECRET_KEY"] = "secret"
 
 boggle_game = Boggle()
 
 
 @app.route("/")
-def show_home_page():
+def homepage():
+    """Show board."""
 
     board = boggle_game.make_board()
-    session["board"] = board
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
 
-    return render_template("home.html", board=board)
+    return render_template("home.html", board=board,
+                           highscore=highscore,
+                           nplays=nplays)
+
 
 @app.route("/check-word")
 def check_word():
+    """Check if word is in dictionary."""
 
     word = request.args["word"]
     board = session["board"]
     response = boggle_game.check_valid_word(board, word)
 
     return jsonify({'result': response})
+
+
+@app.route("/post-score", methods=["POST"])
+def post_score():
+    """Receive score, update nplays, update high score if appropriate."""
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord=score > highscore)
